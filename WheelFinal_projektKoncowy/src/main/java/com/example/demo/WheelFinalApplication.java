33ff85ff351db5d590da62927b3cec3a5399c4bc
Anna Wittmann <annwitt@tlen.pl>
2022-03-12 10:16:02 +0100

    Initial commit

---
+++ WheelFinal_projektKoncowy/src/main/java/com/example/demo/WheelFinalApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WheelFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WheelFinalApplication.class, args);
+	}
+
+}
