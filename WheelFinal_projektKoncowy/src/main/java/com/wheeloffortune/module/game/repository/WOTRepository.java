289cfa65f20ecdc3812b92e52ee7fea84442305a
SirDraq <redwhitedragon81@gmail.com>
2022-03-26 15:59:50 +0100

    Working (15:58)

--- WheelFinal_projektKoncowy/src/main/java/com/wheeloffortune/module/game/repository/WOTRepository.java
+++ WheelFinal_projektKoncowy/src/main/java/com/wheeloffortune/module/game/repository/WOTRepository.java
@@ -3,7 +3,9 @@ package com.wheeloffortune.module.game.repository;
 import com.wheeloffortune.module.game.entity.WOTEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface WOTRepository extends JpaRepository<WOTEntity,Long>, JpaSpecificationExecutor<WOTEntity> {
     public WOTEntity findOneByUuid(String uuid);
 }
--- WheelFinal_projektKoncowy/src/main/java/com/wheeloffortune/module/user/reposytory/UserRepository.java
+++ WheelFinal_projektKoncowy/src/main/java/com/wheeloffortune/module/user/reposytory/UserRepository.java
@@ -3,7 +3,13 @@ package com.wheeloffortune.module.user.reposytory;
 import com.wheeloffortune.module.user.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long>, JpaSpecificationExecutor<UserEntity> {
     public UserEntity findOneByUuid(String uuid);
+
+   public UserEntity findOneByUsername(String username);
 }
